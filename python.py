e79c895a654b07b215dc4f899e27d246b126f3dd
johnnyw <jwu38438@gmail.com>
2023-09-19 18:56:54 -0400

    Update python.py

--- python.py
+++ python.py
@@ -1,4 +1,4 @@
-name=input("What is your name?: ") 
+name=input("Name : ") 
 food=input("Put a food?: ")
 verb1=input("Verb?: ")
 verb2=input("Verb?: ")
@@ -8,5 +8,6 @@ number=input("Number? :")
 cg=input("Celebrity Guest? :")
 adjective=input("Adjective? ") 
 
+Madlib = f"It was {food} day, and {name} was super {adjective} for lunch. But when she went outside to eat, a {noun} stole her {food}! {name} chased the {noun} all over school. She {verb1}, {verb2}, and {verb3} through the playground. Then she tripped on her {noun} and the {noun} escaped! Luckily, {name}s friends were willing to share their {food} with her"
 
-print("It was "+food+" day, and "+name+" was super "+adjective+" for lunch. But when she went outside to eat, a "+noun+" stole her "+food+"! "+name+" chased the "+noun+" all over school. She "+verb1+" ,"+verb2+" ,and "+verb3+"  through the playground. Then she tripped on her "+noun+" and the "+noun+" escaped! Luckily, "+name+"'s friends were willing to share their "+food+" with her. ")
+print(Madlib)
