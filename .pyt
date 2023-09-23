ca9a9340b722197d3b72b577b79a79ed0de4b635
johnnyw <jwu38438@gmail.com>
2023-09-22 20:29:28 -0400

    eg

--- .pyt
+++ .pyt
@@ -1,16 +1,17 @@
 num = int(input("NUMBER : "))
+num2 = int(input("NUMBER 2 : "))
 
 def factor(x):
     print("The factors of ",x," are")
-    for i in range(1, x):
-        if x % 1 
-            print
+    for i in range(1, x + 1 ):
+        if x % i == 0 : 
+            print(i)
 
-factors = factor(num)
+def gcf(a, b): 
+    if (a == 0): 
+        return b
+    else:
+        return gcf(a, a % b)
 
-print(factors) 
-
-
-
-   #rint len(() )``
-
+print("The GCF of ",num," and ",num2," is : ")
+print(gcf(num, num2))
--- python.py
+++ python.py
@@ -1,5 +1,22 @@
-num1 = input("Number: ")
+#madlibs
+name=input("Name : ") 
+food=input("Put a food?: ")
+verb1=input("Verb?: ")
+verb2=input("Verb?: ")
+verb3=input("verb?: ")
+noun=input("Noun? :")
+number=input("Number? :")
+cg=input("Celebrity Guest? :")
+adjective=input("Adjective? ") 
 
-print(num1) 
+Madlib = f"It was {food} day, and {name} was super {adjective} for lunch. But when she went outside to eat, a {noun} stole her {food}! {name} chased the {noun} all over school. She {verb1}, {verb2}, and {verb3} through the playground. Then she tripped on her {noun} and the {noun} escaped! Luckily, {name}s friends were willing to share their {food} with her"
 
- 
+print(Madlib)
+
+#odd/even?
+num=int(input("Number: "))
+
+if (num%2) == 0:
+    print("even")
+else:
+    print("odd")
