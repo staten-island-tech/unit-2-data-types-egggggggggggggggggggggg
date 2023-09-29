d7fe760c1ea0c6db7fed67e81e756b682456d9b5
johnnyw <jwu38438@gmail.com>
2023-09-29 14:00:00 -0400

    egegegegege

--- python.py
+++ python.py
@@ -8,8 +8,28 @@ def gcf(x, y):
 
 print(f"The Greatest Common Factor of {num} and {num2} is " + str(gcf(num, num2)))
 
+total = int(input("Total? "))
+service = str(input("Rate the Service? "))
 
+ge = ("great")
+go = ("good")
+bad = ("bad")
+norm = ("normal")
 
+def tipcalc(x, y): 
+   if x == go: 
+      total = y * 1.15 
+   if x == ge: 
+      total = y * 1.25
+   if x == norm: 
+      total = y * 1.20
+   if x == bad: 
+     total =  y * 1
+print(total)
 
-
+  
+  
+  
+  
+tipcalc(service , total)
 
